3933abb013f3512a9950506f022e10b6077a66a8
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-03-02 19:34:35 +0100

    Add fonts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,35 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
-
 let storageName = "lastStoryboard";
 let defaultStoryboard = JSON.stringify({
   "nice": "nice"
 }, null, "\t");
 
 function init() {
-  global.pdfmake = require('pdfmake/build/pdfmake.js');
-  global.font = require('pdfmake/build/vfs_fonts.js');
-
   let ace = require('brace');
   require('brace/mode/javascript');
   require('brace/theme/monokai');
-   
+  
+  global.pdfmake = require('pdfmake/build/pdfmake.js');
   global.timer = null;
   global.lastChanged = null;
   global.lastGen = null;
   global.editor = ace.edit("editor");
   
-  initEditor();
+  configureEditor();
+  configurePdfMake();
 }
 
-function initEditor() {
+function configurePdfMake() {
+  pdfMake.vfs = require("./vfs_fonts").pdfMake.vfs;
+  pdfMake.fonts = {
+    Roboto: {
+            normal: 'Roboto-Regular.ttf',
+    }
+  };
+}
+
+function configureEditor() {
   let oldSession = localStorage[storageName] ? localStorage[storageName] : defaultStoryboard;
   editor.getSession().setValue(oldSession);
   editor.getSession().setMode('ace/mode/json');
@@ -49,13 +55,10 @@ function generatePdf() {
   let content = editor.getSession().getValue();
   const pdfDocGenerator = pdfMake.createPdf({
     content: content,
-    version: 1.3,
-    vss: {}
   });
   pdfDocGenerator.getDataUrl((dataUrl) => {
-    console.log("ready");
     document.getElementById("pdf-viewer").src = dataUrl;
   });
 }
 
-document.onload = init();
+window.onload = init();
